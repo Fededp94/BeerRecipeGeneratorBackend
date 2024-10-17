43cb3d6c32c786adce94d6a473f73e806927b436
Federico Di Presa <federicodipresa94@gmail.com>
2024-10-17 11:18:36 +0200

    Initial commit

---
+++ src/main/java/beerrecipegenerator/federicodipresa/FedericodipresaApplication.java
@@ -0,0 +1,13 @@
+package beerrecipegenerator.federicodipresa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FedericodipresaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FedericodipresaApplication.class, args);
+	}
+
+}
